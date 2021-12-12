ce0bc7b58a6b6ff73b83b9cd098cb1c04ca8e48b
Gustavo Carvalho <gustavo1net@hotmail.com>
2021-12-12 16:57:31 -0300

    Radix source implementation

--- main.c
+++ main.c
@@ -26,6 +26,47 @@ int inOrder(t_stack *s)
 	return (1);
 }
 
+void big_push_swap(t_stacks *s, int size)
+{
+	int i;
+	int ib;
+
+	ib = 0;
+	while ((!inOrder(&s->a) || s->b.size))
+	{
+		i = 0;
+		while (i < size)
+		{
+			if (((s->a.array[0] >> ib) & 1))
+				rotate_a(s);
+			else
+				push_b(s);
+			i++;
+		}
+		while (s->b.size)
+			push_a(s);
+		ib++;
+	}
+}
+
+int convert_binarie(int num)
+{
+	size_t i;
+	int d;
+	int aux;
+
+	i = 0;
+	d = 1;
+	aux = 0;
+	while (num > 0)
+	{
+		aux += num % 2 * d;
+		d *= 10;
+		num /= 2;
+	}
+	return (aux);
+}
+
 void push_swap(t_stacks *s)
 {
 	int m;
@@ -52,6 +93,28 @@ void push_swap(t_stacks *s)
 	}
 }
 
+void normalize(t_stack *s)
+{
+	size_t i;
+	int minnor;
+
+	i = 0;
+	minnor = 0;
+	while (i < s->size)
+	{
+		if (s->array[i] < minnor)
+			minnor = s->array[i];
+		i++;
+	}
+	i = 0;
+	minnor *= -1;
+	while (i < s->size)
+	{
+		s->array[i] += minnor;
+		i++;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	t_stacks s;
@@ -65,12 +128,20 @@ int main(int argc, char **argv)
 
 	while (i < argc - 1)
 	{
-		s.a.array[i] = ft_atoi(argv[i + 1]);
+		s.a.array[i] = (int)ft_atoi(argv[i + 1]);
 		s.b.array[i] = 0;
 		i++;
 	}
+	i = 0;
+	// while (i < argc - 1)
+	//{
+	//	printf("%d -> %x -> %d\n", s.a.array[i], (s.a.array[i]), (s.a.array[i] >> 0) & 1);
+	//	i++;
+	// }
 	// print_stacks(&s);
-	push_swap(&s);
+	normalize(&s.a);
+	big_push_swap(&s, argc - 1);
+
 	// print_stacks(&s);
 
 	clear(&s);
